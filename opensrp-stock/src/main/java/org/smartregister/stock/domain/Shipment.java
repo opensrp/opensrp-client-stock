6f8c18d8b6342bc4ff14eeae1ad86dad0cc6f7f8
Ephraim Kigamba <ekigamba@ona.io>
2018-03-07 18:26:43 +0300

    Create Shipment & ShipmentLineItem classes
    Co-authored-by: Vincent Karuri <vincentmkaruri@gmail.com>

---
+++ opensrp-stock/src/main/java/org/smartregister/stock/domain/Shipment.java
@@ -0,0 +1,125 @@
+package org.smartregister.stock.domain;
+
+import java.util.Date;
+
+/**
+ * Created by Ephraim Kigamba - ekigamba@ona.io on 07/03/2018.
+ */
+
+public class Shipment {
+
+    private String orderCode;
+    private Date orderedDate;
+    private String receivingFacilityCode;
+    private String receivingFacilityName;
+    private String supplyingFacilityCode;
+    private String supplyingFacilityName;
+    private Date processingPeriodStartDate;
+    private Date processingPeriodEndDate;
+    private String shipmentAcceptStatus;
+    private boolean synced;
+    private ShipmentLineItem[] shipmentLineItems;
+
+    public Shipment(String orderCode, Date orderedDate, String receivingFacilityCode,
+                    String receivingFacilityName, String supplyingFacilityCode, String supplyingFacilityName,
+                    Date processingPeriodStartDate, Date processingPeriodEndDate, String shipmentAcceptStatus, boolean synced) {
+        this.orderCode = orderCode;
+        this.orderedDate = orderedDate;
+        this.receivingFacilityCode = receivingFacilityCode;
+        this.receivingFacilityName = receivingFacilityName;
+        this.supplyingFacilityCode = supplyingFacilityCode;
+        this.supplyingFacilityName = supplyingFacilityName;
+        this.processingPeriodStartDate = processingPeriodStartDate;
+        this.processingPeriodEndDate = processingPeriodEndDate;
+        this.shipmentAcceptStatus = shipmentAcceptStatus;
+        this.synced = synced;
+    }
+
+    public String getOrderCode() {
+        return orderCode;
+    }
+
+    public void setOrderCode(String orderCode) {
+        this.orderCode = orderCode;
+    }
+
+    public Date getOrderedDate() {
+        return orderedDate;
+    }
+
+    public void setOrderedDate(Date orderedDate) {
+        this.orderedDate = orderedDate;
+    }
+
+    public String getReceivingFacilityCode() {
+        return receivingFacilityCode;
+    }
+
+    public void setReceivingFacilityCode(String receivingFacilityCode) {
+        this.receivingFacilityCode = receivingFacilityCode;
+    }
+
+    public String getReceivingFacilityName() {
+        return receivingFacilityName;
+    }
+
+    public void setReceivingFacilityName(String receivingFacilityName) {
+        this.receivingFacilityName = receivingFacilityName;
+    }
+
+    public String getSupplyingFacilityCode() {
+        return supplyingFacilityCode;
+    }
+
+    public void setSupplyingFacilityCode(String supplyingFacilityCode) {
+        this.supplyingFacilityCode = supplyingFacilityCode;
+    }
+
+    public String getSupplyingFacilityName() {
+        return supplyingFacilityName;
+    }
+
+    public void setSupplyingFacilityName(String supplyingFacilityName) {
+        this.supplyingFacilityName = supplyingFacilityName;
+    }
+
+    public Date getProcessingPeriodStartDate() {
+        return processingPeriodStartDate;
+    }
+
+    public void setProcessingPeriodStartDate(Date processingPeriodStartDate) {
+        this.processingPeriodStartDate = processingPeriodStartDate;
+    }
+
+    public Date getProcessingPeriodEndDate() {
+        return processingPeriodEndDate;
+    }
+
+    public void setProcessingPeriodEndDate(Date processingPeriodEndDate) {
+        this.processingPeriodEndDate = processingPeriodEndDate;
+    }
+
+    public boolean isSynced() {
+        return synced;
+    }
+
+    public void setSynced(boolean synced) {
+        this.synced = synced;
+    }
+
+    public String getShipmentAcceptStatus() {
+        return shipmentAcceptStatus;
+    }
+
+    public void setShipmentAcceptStatus(String shipmentAcceptStatus) {
+        this.shipmentAcceptStatus = shipmentAcceptStatus;
+    }
+
+    public ShipmentLineItem[] getShipmentLineItems() {
+        return shipmentLineItems;
+    }
+
+    public void setShipmentLineItems(ShipmentLineItem[] shipmentLineItems) {
+        this.shipmentLineItems = shipmentLineItems;
+    }
+}
---
+++ opensrp-stock/src/main/java/org/smartregister/stock/domain/ShipmentLineItem.java
@@ -0,0 +1,83 @@
+package org.smartregister.stock.domain;
+
+/**
+ * Created by Ephraim Kigamba - ekigamba@ona.io on 07/03/2018.
+ */
+
+public class ShipmentLineItem {
+
+    private int id;
+    private String shipmentOrderCode;
+    private String antigenType;
+    private int orderedQuantity;
+    private int shippedQuantity;
+    private int numberOfDoses;
+    private int acceptedQuantity;
+
+    public ShipmentLineItem(int id, String shipmentOrderCode, String antigenType, int orderedQuantity,
+                            int shippedQuantity, int numberOfDoses, int acceptedQuantity) {
+        this.id = id;
+        this.shipmentOrderCode = shipmentOrderCode;
+        this.antigenType = antigenType;
+        this.orderedQuantity = orderedQuantity;
+        this.shippedQuantity = shippedQuantity;
+        this.numberOfDoses = numberOfDoses;
+        this.acceptedQuantity = acceptedQuantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getShipmentOrderCode() {
+        return shipmentOrderCode;
+    }
+
+    public void setShipmentOrderCode(String shipmentOrderCode) {
+        this.shipmentOrderCode = shipmentOrderCode;
+    }
+
+    public String getAntigenType() {
+        return antigenType;
+    }
+
+    public void setAntigenType(String antigenType) {
+        this.antigenType = antigenType;
+    }
+
+    public int getOrderedQuantity() {
+        return orderedQuantity;
+    }
+
+    public void setOrderedQuantity(int orderedQuantity) {
+        this.orderedQuantity = orderedQuantity;
+    }
+
+    public int getShippedQuantity() {
+        return shippedQuantity;
+    }
+
+    public void setShippedQuantity(int shippedQuantity) {
+        this.shippedQuantity = shippedQuantity;
+    }
+
+    public int getNumberOfDoses() {
+        return numberOfDoses;
+    }
+
+    public void setNumberOfDoses(int numberOfDoses) {
+        this.numberOfDoses = numberOfDoses;
+    }
+
+    public int getAcceptedQuantity() {
+        return acceptedQuantity;
+    }
+
+    public void setAcceptedQuantity(int acceptedQuantity) {
+        this.acceptedQuantity = acceptedQuantity;
+    }
+}
