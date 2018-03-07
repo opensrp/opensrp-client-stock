33bae089f816537787db9aafe1a140f0448ef9d5
Ephraim Kigamba <ekigamba@ona.io>
2018-03-07 18:11:55 +0300

    Implement Order persistence
    1. Add Order constructor
    2. Add Order Constants
    3. Create Order Repository with addOrder, getOrderById & getAllOrders methods
    
    Co-authored-by: Vincent Karuri <vincentmkaruri@gmail.com>

--- opensrp-stock/src/main/java/org/smartregister/stock/util/Constants.java
+++ opensrp-stock/src/main/java/org/smartregister/stock/util/Constants.java
@@ -11,4 +11,18 @@ public class Constants {
     public static final String STEPNAME = "stepName";
 
     public static final String NUMBER_PICKER = "number_picker";
+
+    public static class Order {
+
+        public static final String ID = "id";
+        public static final String REVISION = "revision";
+        public static final String TYPE = "type";
+        public static final String DATE_CREATED = "date_created";
+        public static final String DATE_EDITED = "date_edited";
+        public static final String SERVER_VERSION = "server_version";
+        public static final String LOCATION_ID = "location_id";
+        public static final String PROVIDER_ID = "provider_id";
+        public static final String DATE_CREATED_BY_CLIENT = "date_created_by_client";
+        public static final String SYNCED = "synced";
+    }
 }
