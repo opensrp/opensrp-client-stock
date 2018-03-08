b9b2ec2e6e6d047d17d8a068ff7f6c52ed56edfa
Ephraim Kigamba <ekigamba@ona.io>
2018-03-08 11:03:21 +0300

    Add Shipment & ShipmentLineItem constants
    Add Shipment & ShipmentLineItem table column names as constants
    
    Co-authored-by: Vincent Karuri <vincentmkaruri@gmail.com>

--- opensrp-stock/src/main/java/org/smartregister/stock/util/Constants.java
+++ opensrp-stock/src/main/java/org/smartregister/stock/util/Constants.java
@@ -11,4 +11,29 @@ public class Constants {
     public static final String STEPNAME = "stepName";
 
     public static final String NUMBER_PICKER = "number_picker";
+
+    public static class Shipment {
+
+        public static final String ORDER_CODE = "order_code";
+        public static final String ORDERED_DATE = "ordered_date";
+        public static final String RECEIVING_FACILITY_CODE = "receiving_facility_code";
+        public static final String RECEIVING_FACILITY_NAME = "receiving_facility_name";
+        public static final String SUPPLYING_FACILITY_CODE = "supplying_facility_code";
+        public static final String SUPPLYING_FACILITY_NAME = "supplying_facility_name";
+        public static final String PROCESSING_PERIOD_START_DATE = "processing_period_start_date";
+        public static final String PROCESSING_PERIOD_END_DATE = "processing_period_end_date";
+        public static final String SHIPMENT_ACCEPT_STATUS = "shipment_accept_status";
+        public static final String SYNCED = "synced";
+    }
+
+    public static class ShipmentLineItem {
+
+        public static final String ID = "id";
+        public static final String SHIPMENT_ORDER_CODE = "shipment_order_code";
+        public static final String ANTIGEN_TYPE = "antigen_type";
+        public static final String ORDERED_QUANTITY = "ordered_quantity";
+        public static final String SHIPPED_QUANTITY = "shipped_quantity";
+        public static final String NUMBER_OF_DOSES = "number_of_doses";
+        public static final String ACCEPTED_QUANTITY = "accepted_quantity";
+    }
 }
