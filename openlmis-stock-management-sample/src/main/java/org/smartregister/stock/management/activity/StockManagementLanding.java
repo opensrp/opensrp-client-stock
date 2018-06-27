e85d085aac0810baed41771e40c76846da1d8d5e
Vincent Karuri <vincentmkaruri@gmail.com>
2018-06-27 12:08:43 +0300

    Add main landing activity

---
+++ openlmis-stock-management-sample/src/main/java/org/smartregister/stock/management/activity/StockManagementLanding.java
@@ -0,0 +1,15 @@
+package org.smartregister.stock.management.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.smartregister.stock.management.R;
+
+public class StockManagementLanding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.stock_management_landing);
+    }
+}
