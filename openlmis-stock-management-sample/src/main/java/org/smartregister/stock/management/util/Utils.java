5def7a99da0158a474b29b14bd04d813c9bb3cb3
Vincent Karuri <vincentmkaruri@gmail.com>
2018-06-29 15:34:17 +0300

    Add util

--- openlmis-stock-management-sample/src/main/java/org/smartregister/stock/management/util/Utils.java
+++ openlmis-stock-management-sample/src/main/java/org/smartregister/stock/management/util/Utils.java
@@ -3,4 +3,8 @@ package org.smartregister.stock.management.util;
 public class Utils {
 
     public static final String INSERT_OR_REPLACE = "INSERT OR REPLACE INTO %s VALUES ";
+
+    public static Boolean convertIntToBoolean(int i) {
+        return i > 0;
+    }
 }
