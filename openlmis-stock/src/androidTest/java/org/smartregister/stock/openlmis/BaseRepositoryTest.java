47b1402f57b3744cd87f62a867214058ceb26225
Samuel Githengi <samuel.githengi@gmail.com>
2018-07-11 10:29:46 +0300

    Add Mock Application, Repository and custom AndroidJUnitRunner for instrumented tests

--- openlmis-stock/src/androidTest/java/org/smartregister/stock/openlmis/BaseRepositoryTest.java
+++ openlmis-stock/src/androidTest/java/org/smartregister/stock/openlmis/BaseRepositoryTest.java
@@ -1,24 +1,24 @@
 package org.smartregister.stock.openlmis;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.smartregister.repository.Repository;
-import org.smartregister.stock.management.application.Application;
+import org.smartregister.stock.openlmis.mock.TestApplication;
 
 @RunWith(AndroidJUnit4.class)
 public abstract class BaseRepositoryTest {
 
     protected static Context context;
     protected static Repository mainRepository;
+    protected static org.smartregister.Context openSRPContext;
 
     @BeforeClass
     public static void bootStrap() {
-        context = InstrumentationRegistry.getTargetContext();
-        Application.setAppContext(context);
-        mainRepository = Application.getInstance().getRepository();
+        context = TestApplication.getInstance().getApplicationContext();
+        mainRepository = TestApplication.getInstance().getRepository();
+
     }
 }
--- openlmis-stock/src/androidTest/java/org/smartregister/stock/openlmis/mock/TestApplication.java
+++ openlmis-stock/src/androidTest/java/org/smartregister/stock/openlmis/mock/TestApplication.java
@@ -1,15 +1,16 @@
-package org.smartregister.stock.management.application;
+package org.smartregister.stock.openlmis.mock;
 
 import org.smartregister.Context;
 import org.smartregister.CoreLibrary;
 import org.smartregister.repository.Repository;
-import org.smartregister.stock.openlmis.OpenLMISLibrary;
-import org.smartregister.stock.openlmis.repository.StockManagementRepository;
 import org.smartregister.view.activity.DrishtiApplication;
 
 import static org.smartregister.util.Log.logError;
 
-public class Application extends DrishtiApplication {
+/**
+ * Created by samuelgithengi on 7/10/18.
+ */
+public class TestApplication extends DrishtiApplication {
 
     @Override
     public void onCreate() {
@@ -19,21 +20,14 @@ public class Application extends DrishtiApplication {
         context = Context.getInstance();
         context.updateApplicationContext(getApplicationContext());
         CoreLibrary.init(context);
-
-        //Initialize OpenLMISLibrary
-        OpenLMISLibrary.init(context, getRepository());
-    }
-
-
-    public static synchronized Application getInstance() {
-        return (Application) mInstance;
     }
 
     @Override
     public Repository getRepository() {
         try {
             if (repository == null) {
-                repository = new StockManagementRepository(getApplicationContext(), context);
+                repository = new TestRepository(getApplicationContext(), context.session(), null,
+                        context.sharedRepositoriesArray());
             }
         } catch (UnsatisfiedLinkError e) {
             logError("Error on getRepository: " + e);
@@ -42,8 +36,12 @@ public class Application extends DrishtiApplication {
     }
 
     @Override
+    public String getPassword() {
+        return "Testing_Pass";
+    }
+
+    @Override
     public void logoutCurrentUser() {
 
     }
-
 }
