acb24ddfb043255520f0c84c85904d09cae92eb4
Samuel <samuel.githengi@gmail.com>
2018-07-23 15:28:32 +0300

    Code cleanup

--- openlmis-stock/src/main/java/org/smartregister/stock/openlmis/view/StockListActivity.java
+++ openlmis-stock/src/main/java/org/smartregister/stock/openlmis/view/StockListActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
