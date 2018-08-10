858dbd42418cd0badd20ae17225fbe2780d6ad3f
Vincent Karuri <vincentmkaruri@gmail.com>
2018-08-10 18:41:12 +0300

    Add LotSyncIntentService logic and tests

--- openlmis-sample/src/androidTest/java/org/smartregister/stock/management/intent/LotSyncIntentServiceTest.java
+++ openlmis-sample/src/androidTest/java/org/smartregister/stock/management/intent/LotSyncIntentServiceTest.java
@@ -1,4 +1,44 @@
 package org.smartregister.stock.management.intent;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.internal.runners.JUnit4ClassRunner;
+import org.junit.runner.RunWith;
+import org.smartregister.stock.management.application.Application;
+import org.smartregister.stock.openlmis.OpenLMISLibrary;
+import org.smartregister.stock.openlmis.intent.LotSyncIntentService;
+import org.smartregister.stock.openlmis.repository.openlmis.LotRepository;
+
+import java.util.concurrent.TimeUnit;
+
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.fail;
+import static org.smartregister.stock.management.util.ServiceUtils.startService;
+import static org.smartregister.stock.management.util.ServiceUtils.stopService;
+
+@RunWith(JUnit4ClassRunner.class)
 public class LotSyncIntentServiceTest {
+
+    LotRepository repository = OpenLMISLibrary.getInstance().getLotRepository();
+
+    @Before
+    public void setup() {
+        startService(Application.getInstance().getApplicationContext(), LotSyncIntentService.class);
+    }
+
+    @After
+    public void tearDown() {
+        stopService(Application.getInstance().getApplicationContext(), LotSyncIntentService.class);
+    }
+
+    @Test
+    public void testLotsAreSyncedAndSaved() {
+        try {
+            TimeUnit.SECONDS.sleep(5);
+            assertNotNull(repository.findLotById("id"));
+        } catch (InterruptedException e) {
+            fail("Waiting for the worker thread took too long.");
+        }
+    }
 }
--- openlmis-stock/src/main/java/org/smartregister/stock/openlmis/intent/LotSyncIntentService.java
+++ openlmis-stock/src/main/java/org/smartregister/stock/openlmis/intent/LotSyncIntentService.java
@@ -10,10 +10,10 @@ import android.util.Log;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import org.json.JSONArray;
 import org.smartregister.domain.Response;
-import org.smartregister.repository.AllSharedPreferences;
+import org.smartregister.service.ActionService;
 import org.smartregister.service.HTTPAgent;
-import org.smartregister.stock.StockLibrary;
 import org.smartregister.stock.openlmis.OpenLMISLibrary;
 import org.smartregister.stock.openlmis.R;
 import org.smartregister.stock.openlmis.domain.openlmis.Lot;
@@ -21,17 +21,16 @@ import org.smartregister.stock.openlmis.repository.openlmis.LotRepository;
 import org.smartregister.stock.util.NetworkUtils;
 
 import java.text.MessageFormat;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.smartregister.util.Log.logError;
-import static org.smartregister.util.Log.logInfo;
 
 public class LotSyncIntentService extends IntentService implements SyncIntentService  {
 
     private static final String LOT_SYNC_URL = "rest/lots/sync";
     private Context context;
     private HTTPAgent httpAgent;
+    private ActionService actionService;
 
     public LotSyncIntentService() {
         super("LotSyncIntentService");
@@ -40,17 +39,13 @@ public class LotSyncIntentService extends IntentService implements SyncIntentSer
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         context = getBaseContext();
-        httpAgent = StockLibrary.getInstance().getContext().getHttpAgent();
+        actionService =OpenLMISLibrary.getInstance().getContext().actionService();
+        httpAgent = OpenLMISLibrary.getInstance().getContext().getHttpAgent();
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     protected void onHandleIntent(Intent workIntent) {
-
-        if (OpenLMISLibrary.getInstance().getContext().IsUserLoggedOut()) {
-            logInfo("Not updating from server as user is not logged in.");
-            return;
-        }
         if (NetworkUtils.isNetworkAvailable(context)) {
             pullFromServer();
         }
@@ -61,55 +56,67 @@ public class LotSyncIntentService extends IntentService implements SyncIntentSer
 
         final String PREV_SYNC_SERVER_VERSION = "prev_sync_server_version";
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-        AllSharedPreferences allSharedPreferences = new AllSharedPreferences(preferences);
-        String baseUrl = OpenLMISLibrary.getInstance().getContext().configuration().dristhiBaseURL();
 
+        String baseUrl = OpenLMISLibrary.getInstance().getContext().configuration().dristhiBaseURL();
         if (baseUrl.endsWith(context.getString(R.string.url_separator))) {
             baseUrl = baseUrl.substring(0, baseUrl.lastIndexOf(context.getString(R.string.url_separator)));
         }
 
-        long timestamp = preferences.getLong(PREV_SYNC_SERVER_VERSION, 0);
-        String timeStampString = String.valueOf(timestamp);
-        String uri = MessageFormat.format("{0}/{1}?serverVersion={3}",
-                baseUrl,
-                LOT_SYNC_URL,
-                timeStampString
-        );
+        while (true) {
+            long timestamp = preferences.getLong(PREV_SYNC_SERVER_VERSION, 0);
+            String timeStampString = String.valueOf(timestamp);
 
-        Response<String> response = httpAgent.fetch(uri);
-        if (response.isFailure()) {
-            logError("Stock pull failed.");
-            return;
-        }
+            baseUrl = "http://10.20.25.188:8080/openlmis"; // TODO REMOVE THIS
+            timeStampString = "0"; // TODO REMOVE THIS
 
-        String jsonPayload = response.payload();
-        List<Lot> lots = getLotsFromPayload(jsonPayload);
+            String uri = MessageFormat.format("{0}/{1}?sync_server_version={2}",
+                    baseUrl,
+                    LOT_SYNC_URL,
+                    timeStampString
+            );
 
-        // store lots
-        Long highestTimeStamp = 0L;
-        LotRepository repository = OpenLMISLibrary.getInstance().getLotRepository();
-        for (int i = 0; i < lots.size(); i++) {
-            Lot lot = lots.get(i);
-            repository.addOrUpdate(lot);
-            if (lot.getServerVersion() > highestTimeStamp) {
-                highestTimeStamp = lot.getServerVersion();
+            try {
+                Response<String> response = httpAgent.fetch(uri);
+                if (response.isFailure()) {
+                    logError("Lots pull failed.");
+                    return;
+                }
+
+                String jsonPayload = response.payload();
+                // List<Lot> lots = getLotsFromPayload(jsonPayload);
+
+                // store lots
+                Long highestTimeStamp = 0L;
+                JSONArray lotsArray = new JSONArray(jsonPayload);
+
+                List<Lot> lots = new Gson().fromJson(jsonPayload, new TypeToken<List<Lot>>(){}.getType());
+
+                LotRepository repository = OpenLMISLibrary.getInstance().getLotRepository();
+                for (int i = 0; i < lotsArray.length(); i++) {
+                    Lot lot = convertToLot(lotsArray.getJSONObject(i).toString());
+                    repository.addOrUpdate(lot);
+                    if (lot.getServerVersion() > highestTimeStamp) {
+                        highestTimeStamp = lot.getServerVersion();
+                    }
+                }
+
+                // save highest server version
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putLong(PREV_SYNC_SERVER_VERSION, highestTimeStamp);
+                editor.commit();
+            } catch (Exception e) {
+                logError(e.getMessage());
             }
         }
-
-        // save highest server version
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putLong(PREV_SYNC_SERVER_VERSION, highestTimeStamp);
-        editor.commit();
     }
 
-    private List<Lot> getLotsFromPayload(String jsonPayload) {
+    private Lot convertToLot(String lotObj) {
 
-        List<Lot> lots = new ArrayList<>();
         try {
-            return new Gson().fromJson(jsonPayload, new TypeToken<List<Lot>>(){}.getType());
+            return new Gson().fromJson(lotObj, new TypeToken<Lot>(){}.getType());
         } catch (Exception e) {
             Log.e(getClass().getCanonicalName(), e.getMessage());
+            return null;
         }
-        return lots;
     }
 }
