1aadd01bfc4d48699896f8ad57656e608339f329
Vincent Karuri <vincentmkaruri@gmail.com>
2018-09-04 18:02:21 +0300

    Add localhost and port to app.properties and add logging

--- openlmis-sample/src/main/java/org/smartregister/stock/management/receiver/OnBootReceiver.java
+++ openlmis-sample/src/main/java/org/smartregister/stock/management/receiver/OnBootReceiver.java
@@ -3,16 +3,19 @@ package org.smartregister.stock.management.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 import org.smartregister.stock.management.application.Application;
 
 public class OnBootReceiver extends BroadcastReceiver {
 
     private final Intent intent = new Intent();
+    private final String TAG = OnBootReceiver.class.getCanonicalName();
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            Log.d(TAG, "Boot completed intent received in OpenLMIS onReceive");
             intent.putExtra(Intent.ACTION_BOOT_COMPLETED, true);
             this.restartAlarms(context);
         }
@@ -20,5 +23,6 @@ public class OnBootReceiver extends BroadcastReceiver {
 
     private void restartAlarms(Context context) {
         Application.setAlarms(context);
+        Log.d(TAG, "Alarms restarted in OpenLMIS");
     }
 }
