5ecece176cb905ed48e01f5683d6c431ea6be422
Simon Njoroge <snjoroge@ona.io>
2020-12-04 10:00:19 +0300

    Add SyncConfiguration in sample apps

---
+++ openlmis-sample/src/main/java/org/smartregister/stock/management/application/SampleSyncConfiguration.java
@@ -0,0 +1,78 @@
+package org.smartregister.stock.management.configuration;
+
+import org.smartregister.SyncConfiguration;
+import org.smartregister.SyncFilter;
+import org.smartregister.view.activity.BaseLoginActivity;
+
+import java.util.Collections;
+import java.util.List;
+
+public class SampleSyncConfiguration extends SyncConfiguration {
+
+    @Override
+    public int getSyncMaxRetries() {
+        return 0;
+    }
+
+    @Override
+    public SyncFilter getSyncFilterParam() {
+        return SyncFilter.LOCATION;
+    }
+
+    @Override
+    public String getSyncFilterValue() {
+//        AllSharedPreferences sharedPreferences = SampleApplication.getInstance().getContext().allSharedPreferences();
+//        return sharedPreferences.fetchDefaultTeamId(sharedPreferences.fetchRegisteredANM());
+        return null;
+    }
+
+    @Override
+    public int getUniqueIdSource() {
+        return 0;
+    }
+
+    @Override
+    public int getUniqueIdBatchSize() {
+        return 0;
+    }
+
+    @Override
+    public int getUniqueIdInitialBatchSize() {
+        return 0;
+    }
+
+    @Override
+    public SyncFilter getEncryptionParam() {
+        return SyncFilter.LOCATION;
+    }
+
+    @Override
+    public boolean updateClientDetailsTable() {
+        return false;
+    }
+
+    @Override
+    public List<String> getSynchronizedLocationTags() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getTopAllowedLocationLevel() {
+        return "";
+    }
+
+    @Override
+    public String getOauthClientId() {
+        return null;
+    }
+
+    @Override
+    public String getOauthClientSecret() {
+        return null;
+    }
+
+    @Override
+    public Class<? extends BaseLoginActivity> getAuthenticationActivity() {
+        return null;
+    }
+}
